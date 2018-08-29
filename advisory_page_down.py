5cee422f04aff9d421b697ada5b6b50f8ee8e849
Xie Shiwen <2@cug.edu.cn>
2018-08-29 21:02:40 +0800

    优化程序效率
    1、构造二维数组避免反复加载咨询页 title 和 url 所在页面
    2、注释掉部分 sleep
    3、保存为 TXT 文件而不是 htm，反复测试好像 TXT 效率更高一点

--- advisory_page_down.py
+++ advisory_page_down.py
@@ -16,11 +16,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 # 定义抓取数据的url，时间跨度及存储路径等初始值
 BASE_URL = 'https://www.haodf.com/sitemap-zx/'
-DATE_START = '20080214'
-DATE_END = '20080214'
+DATE_START = '20080222'
+DATE_END = '20080222'
 DIR_PATH = './'
 TIME_WAIT = 10
-TIME_SLEEP = 2
+TIME_SLEEP = 0.5
 # log 编码方式
 ENCODING_STYLE = 'gb18030'
 
@@ -49,28 +49,20 @@ def down_detail_page(file_path, local_time):
         # 该函数内部调用获取详情页源码的函数 get_detail_page
         # =====================
         # 首先考虑生成时间戳，然后取前后两次的差，如果差小于某个值，说明可能存在拒绝访问、
-        # 网速差或其他未知错误，抓取页面不正常，但程序仍在无效运行
+        # 网速差或其他未知错误，抓取页面不正常，但程序仍在无效运行，可能出现在抓取日期页过程中
         # =====================
-        # try:
-        # 用 time.perf_counter()方法去当前的系统时间，精确到秒，然后在 create_date_page_url()
-        # 运行结束后再取一次系统时间，然后取差值判断是否大于某个值，如果小于，则停止程序并反馈信息
         start_time = time.perf_counter()
         # 调用函数生成某一天的每一页页面 url，然后解析获取该页面中的 title 和 url
         creat_date_page_url(advisory_date, file_path, local_time)
         # 输出浮点型数据
         delta_time = time.perf_counter() - start_time
-        # 假设网断了，至少TIME_WAIT 时间；
-        # 结束 IP 被封，至少TIME_WAIT()时间
-        # 较短时间成功完成（该页空白无 title）： 至少 TIME_SLEEP 时间
-        # 所以设置完成某天抓取小于多少时间或大于多少时间意义不大，可以将时差打印出来，人来判断手工终止程序
+        # 这里设置完成某天抓取小于多少时间或大于多少时间意义不大，可以将时差打印出来，人工来判断手工终止程序
         # 暂时不清楚 browser.get()会不会触发异常
-        # if delta_time < float()
-        # 本来在一行，delta_time 有 warning
+        # 以下本来在一行，delta_time 有 warning
         print(advisory_date.strftime('%Y-%m-%d'), ' 日的页面用时  ', end='')
         print(delta_time, end='')
         print('  秒解析并抓取完毕')
         advisory_date += datetime.timedelta(days=1)  # 日期推进一天，联系下文不用加 time.sleep
-        # except Exception:
     else:
         print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), ' 程序顺利运行结束!')
 
@@ -90,8 +82,8 @@ def creat_date_page_url(advisory_date, file_path, local_time):
         print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), ' 开始尝试抓取 ', advisory_date.strftime('%Y-%m-%d'),
               ' 日第 ', str(date_page), ' 页问诊记录')
         try:
-            browser.get(date_page_url)  # 获取含 title 和detail page 的页面
-            # 页面是否加载成功
+            browser.get(date_page_url)  # 获取含 title 和 detail page url的页面
+            #  等页面加载成功直到时间超过 TIME_WAIT
             wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="map_all"]')))
             try:
                 # 查找页面class name为'hh'的节点。这里也可以用 try except 做
@@ -99,24 +91,23 @@ def creat_date_page_url(advisory_date, file_path, local_time):
                 browser.find_element_by_xpath('//li[@class="hh"]')
                 # 和 html.xpath 获取 text()不同。用 elements
                 item = browser.find_elements_by_xpath('//li/a')
+                # 取详情页数量
+                len_item = len(item)
+                # 将 item 的属性值即 title 和 url 存入二维数组中，调用函数creat_arr_title_url（）
+                arr_title_url = creat_arr_title_url(item, len_item)
                 # 生成最后网页文件名称前缀
                 pre_file_name = advisory_date.strftime('%Y%m%d') + '_' + str(date_page) + '_'
-                for i in range(len(item)):
+                for i in range(len_item):
                     # 打印状态
                     print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), advisory_date.strftime('%Y-%m-%d'),
-                          ' 日第 ', str(date_page), ' 页共有 ', str(len(item)), ' 条问诊记录，正在抓取第 ', str(i+1), ' 条')
-                    # 考虑将全部将 item_title 和 item_url存入 TXT 文本
+                          ' 日第 ', str(date_page), ' 页共有 ', str(len_item), ' 条问诊记录，正在抓取第 ', str(i+1), ' 条')
                     # 记录所有成功加载的某日某页面中所有的 title 和 url，包含可能将没有成功保存至本地的
                     record_title_url_filename = 'TitleandUrl' + DATE_START + DATE_END + local_time
                     with open(file_path + 'log/' + record_title_url_filename + '.txt', 'a', encoding=ENCODING_STYLE) \
                             as record_title_url:
-                        record_title_url.write(item[i].get_attribute('href') + '\t' + item[i].text + '\n')
+                        record_title_url.write(arr_title_url[i][0] + '\t' + arr_title_url[i][1] + '\n')
                     # 调用函数获取某一日某一页上所有医患对话详情页 URL 对应的页面并存入本地
-                    get_detail_page(item[i].get_attribute('href'), pre_file_name, file_path, local_time)
-                    # 以下需要重新加载，不然下一次循环就会解析get_detail_page中 get 的网页
-                    # 将 item 的 href 和 text 取出来存入中间量，数据库等可不用重载入，当前访问过于频繁
-                    browser.get(date_page_url)
-                    item = browser.find_elements_by_xpath('//li/a')
+                    get_detail_page(arr_title_url[i][0], pre_file_name, file_path, local_time)
                 # 记录含有 医患对话 title 和 url的 date_page_url
                 normal_date_page_url = 'NormalDatePageUrl' + DATE_START + DATE_END + local_time
                 with open(file_path + 'log/' + normal_date_page_url + '.txt', 'a', encoding=ENCODING_STYLE) \
@@ -148,11 +139,24 @@ def creat_date_page_url(advisory_date, file_path, local_time):
             break
 
 
+def creat_arr_title_url(item, len_item):
+    """
+    解析出当前页中的所有 title 和 url，并存入二维数组
+    :param item:
+    :param len_item:
+    :return: arr_title_url
+    """
+    # 数组初始化，行数为节点的个数
+    arr_title_url = [[] for j in range(len_item)]
+    for i in range(len_item):
+        arr_title_url[i].append(item[i].get_attribute('href'))
+        arr_title_url[i].append(item[i].text)
+    return arr_title_url
+
+
 def get_detail_page(detail_page_url, pre_file_name, file_path, local_time):
     """
     获取 某一页中所有 title 和 url对应的医患对话详情页，并保存到本地
-    本来函数有一个循环，传入的是 iterm，当时上一个函数的 iterm 和 browser.get 没有在循环中，引起量的变化，总是报错
-    于是我将本函数改为单url 调用，在上一个函数中加入了 iterm 和 browse.get 的重载入
     :param detail_page_url:
     :param pre_file_name:
     :param file_path:
@@ -160,7 +164,7 @@ def get_detail_page(detail_page_url, pre_file_name, file_path, local_time):
     :return:
     """
     try:
-        # 注意 href 值的'//'问题，暂未处理，可考虑用 xpath 查找后，用.get_attribute的方法获取 url
+        # 注意 href 值的'//'问题，暂未处理
         browser.get(detail_page_url)
         # 等待所有节点加载出来
         wait.until(EC.presence_of_all_elements_located)
@@ -170,8 +174,8 @@ def get_detail_page(detail_page_url, pre_file_name, file_path, local_time):
         # HTML 命名形如20180322_1_xxx.htm,以下用切片的方法获取没有'/'的部分，不然会被认为是路径
         # 切片也可以用 detail_page_url.split('/')[-1]
         # file_name = pre_file_name + detail_page_url.split('/')[-1]
-        file_name = pre_file_name + detail_page_url[28:]
-        # 命名记录所有成功保存至本地的网页的名称
+        file_name = pre_file_name + detail_page_url[28:] + '.txt'
+        # 命名记录所有成功保存至本地的网页的名称的文本
         record_filename_name = 'NameofSavedPages' + DATE_START + DATE_END + local_time
         # 保存网页源码到本地， file_name 自带'.htm' 后缀，采用和网页相同的 gbk 编码
         with open(file_path + file_name, 'w', encoding='gbk') as file:
@@ -191,7 +195,6 @@ def get_detail_page(detail_page_url, pre_file_name, file_path, local_time):
             record_errfilename.write(pre_file_name + '_' + detail_page_url + '\n')
         # 2018-08-14 2140
         # time.sleep(TIME_SLEEP)
-        # browser.close()
 
 
 def make_dir():
@@ -220,7 +223,7 @@ def main():
     local_time = time.strftime('%Y%m%d_%H%M%S', time.localtime())
     # 可以考虑给文件夹名称也接一个 local_time 标识
     # ========================================================
-    # 当前路径设置要求，如果程序异常终止，需要人工删除系列 log 文件
+    # 当前路径设置要求，如果程序因异常终止，需要人工删除系列 log 文件
     # ========================================================
     file_path = make_dir()
     try:
@@ -233,3 +236,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
